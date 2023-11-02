ad09b12f674353b7ede60f26410ae35221be9706
AbdumalikBebeza <sultan.bebeza@gmail.com>
2023-11-02 13:25:55 +0600

    blablaabla

--- handlers/FSM/send_to_tg_channel.py
+++ handlers/FSM/send_to_tg_channel.py
@@ -20,12 +20,11 @@ class SendToChannelFSM(StatesGroup):
 
 media_group = types.MediaGroup()
 
+
 async def fsm_start(message: types.Message):
     if message.from_user.id in Admins:
         await SendToChannelFSM.title.set()
         await message.answer('Введите заголовок для рассылки в канал', reply_markup=buttons.cancel_markup)
-    else:
-        await message.answer("Вы не админ!!!")
 
 
 async def load_title(message: types.Message, state: FSMContext):
@@ -62,9 +61,12 @@ async def finish_load_photos(message: types.Message, state: FSMContext):
                 pass
             media_group.attach_photo(types.InputMediaPhoto(i))
 
+        media_group[-1] = media_group.attach_photo(types.InputMediaPhoto(
+            i, caption=f"{data['title_for_channel']}\n\n{data['text_for_channel']}"))
 
-        print(media_group)
+        print(media_group[-1])
         await bot.send_media_group(chat_id=message.from_user.id, media=media_group)
+
         await message.answer("Всё правильно?", reply_markup=buttons.submit_markup)
         await SendToChannelFSM.next()
 
@@ -72,10 +74,9 @@ async def finish_load_photos(message: types.Message, state: FSMContext):
 async def load_submit(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         if message.text.lower() == 'да':
-            channel_id = CHANNEL_ID
-            await bot.send_media_group(chat_id=channel_id, media=media_group)
-            # await bot.send_message(chat_id=channel_id, text=f"{data['title_for_channel']}\n\n{data['text_for_channel']}")
+            await bot.send_media_group(chat_id=CHANNEL_ID, media=media_group)
             await state.finish()
+            media_group.clean()
         else:
             await message.answer("Отмена!")
             await state.finish()
@@ -90,7 +91,7 @@ async def cancel_reg(message: types.Message, state: FSMContext):
 
 def register_send_to_channel(dp: Dispatcher):
     dp.register_message_handler(cancel_reg, Text(equals='Отмена', ignore_case=True), state='*')
-    dp.register_message_handler(fsm_start, commands=['news'])
+    dp.register_message_handler(fsm_start, commands=['рассылка'])
 
     dp.register_message_handler(load_title, state=SendToChannelFSM.title)
     dp.register_message_handler(load_text, state=SendToChannelFSM.text)
--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 from aiogram.utils import executor
 import logging
-<<<<<<< HEAD
 from config import dp, bot, Admins, Director
 from handlers.start import register_start
-
-# ===========================================================================
-async def on_startup(_):
-    for i in Admins:
-        await bot.send_message(chat_id=i, text="Бот запущен!")
-    # await bot.send_message(chat_id=Director[0], text="Бот запущен!", reply_markup=buttons.start_director_markup)
-
-
-register_start(dp)
-
-# ===========================================================================
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
-=======
-from config import dp, bot, Admins
-from handlers import commands
 from handlers.FSM import review_client, send_to_tg_channel
 
 
@@ -30,8 +12,7 @@ async def on_startup(_):
     # await bot.send_message(chat_id=Director[0], text="Бот запущен!", reply_markup=buttons.start_director_markup)
 
 
-commands.register_start(dp)
-
+register_start(dp)
 review_client.register_review(dp)
 send_to_tg_channel.register_send_to_channel(dp)
 
@@ -39,4 +20,3 @@ send_to_tg_channel.register_send_to_channel(dp)
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
->>>>>>> 506a49e5f92c438bf38702a0d26fca668305c158
